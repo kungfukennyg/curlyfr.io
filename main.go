83f30d43cd274f38c1ce4a7d7cb5455f67a6d302
Kenny Goodin <kennethbgoodin@gmail.com>
2025-01-25 12:20:33 -0500

    main.go: log requests

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"os"
 )
+
+var fs = http.FileServer(http.Dir("./public"))
 
 // serves static files from a public directory. expects to be deployed behind a
 // public-facing load balancer, like nginx, with TLS terminated prior at the
 // load balancer.
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
+	http.Handle("/", loggingHandler{})
 
 	fmt.Println("Listening on :1024...")
 	err := http.ListenAndServe(":1024", fs)
@@ -19,4 +21,19 @@ func main() {
 		fmt.Printf("failed to listen: %v\n", err)
 		os.Exit(1)
 	}
+}
+
+type loggingHandler struct{}
+
+func (lh loggingHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	log.Printf("[WEB] got request for '%s' from '%s\n", req.URL.EscapedPath(), remoteAddr(req))
+	fs.ServeHTTP(w, req)
+}
+
+func remoteAddr(req *http.Request) string {
+	if addr := req.Header.Get("X-FORWARDED-FOR"); addr != "" {
+		return addr
+	}
+
+	return req.RemoteAddr
 }
